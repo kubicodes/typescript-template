2da70737d9187e0c3b78bf27e07cf626f692d791
Kubi <kubilayercikti1@gmail.com>
2023-08-05 18:26:46 +0200

    Use singleton for ajv

--- src/shared/schema-validation/schema-validation.service.ts
+++ src/shared/schema-validation/schema-validation.service.ts
@@ -5,9 +5,13 @@ export interface SchemaValidationService {
 }
 
 export class AjvSchemaValidationService<T> implements SchemaValidationService {
-    public getValidationFunction(schema: Schema): ValidateFunction {
-        const ajv = new Ajv();
+    private ajvInstance: Ajv;
 
-        return ajv.compile<T>(schema);
+    public getValidationFunction(schema: Schema): ValidateFunction {
+        if (!this.ajvInstance) {
+            this.ajvInstance = new Ajv();
+        }
+
+        return this.ajvInstance.compile<T>(schema);
     }
 }
